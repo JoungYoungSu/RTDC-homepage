d473176476338b21f9b716734ad67ff45fcbf793
neogulmon <jeongysu@gmail.com>
2022-09-28 15:15:03 +0900

    기본 구조 적용

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-var express = require("express");
- 
-//Express 호출 후 새로운 Express 애플리케이션을 변수(app)에 넣음
-var app = express();
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
 app.get("/", function(request, response){
     response.send("hello world!");
 });
 
-app.listen(80, function(){
-    console.log("Express app running on port : 80");
+// Router 설정
+const testRouter = require("./router/test.router");
+
+// Router 주소 지정
+app.use('/test', testRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(CODE.NOT_FOUND));
+    next();
+  });
+
+app.listen(PORT, function(){
+    console.log("Express app running on port : ", PORT);
 }); 
